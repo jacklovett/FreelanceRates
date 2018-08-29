28ad06f94f1fc069f8a0f2c124b1ea36e688a9b9
Jack Lovett <jack.l.lovett@gmail.com>
2018-08-29 23:07:07 +0200

    Student plan functionality
    Fixing student plan switchs functionality and adding example of how to get data to results page.

--- app/controllers/mainController.py
+++ app/controllers/mainController.py
@@ -4,18 +4,22 @@ class MainController():
         self.desired_salary = form.get('desired_salary', type=int)
         self.billable_hours = form.get('billable_hours', type=int)
         self.time_off = form.get('time_off', type=int)
-        self.student_loan_plan = self.determine_student_loan_plan()        
+        self.student_loan_plan = self.determine_student_loan_plan(form)        
         
     def calc_result(self):
-        print(self.form.get('desired_salary'))
+        result = {}
+        result['desired_salary'] = self.form.get('desired_salary')
+        result['billable_hours'] = self.form.get('billable_hours')
+        result['time_off'] = self.form.get('time_off')
+        result['student_loan_plan'] = self.student_loan_plan
+        return result
     
-    def determine_student_loan_plan(self):
-        input_form = self.form
-        has_student_loan = input_form.get('plan_toggle', type=bool)
-        if has_student_loan == 'on':
-            if input_form.get('plan_1', type=bool) == 'on':
+    def determine_student_loan_plan(self, form):
+        has_student_loan = form.get('plan_toggle', type=bool)
+        if has_student_loan:
+            if form.get('plan_1', type=bool):
                 return 1            
-            if input_form.get('plan_2', type=bool) == 'on':
+            if form.get('plan_2', type=bool):
                 return 2        
         else:
             return 0
--- app/static/js/studentLoanController.js
+++ app/static/js/studentLoanController.js
@@ -2,17 +2,43 @@
 window.onload = studentLoanController();
 
 function studentLoanController() {
-	try	{
-		var row = document.getElementById("row");
-		var planToggle = document.querySelector("input[name=plan_toggle]");
-		    	
-		planToggle.onchange = function() {
-			if(this.checked)
-				row.style.display = "";				
-			else 
-				row.style.display = "none";								
-		};
+	
+	var row, plan1, plan2, planToggle;
+	
+	try {
+		row = document.getElementById("row");
+		planToggle = document.querySelector("input[name=plan_toggle]");
+		// radio buttons
+		plan1 = document.getElementById("plan_1");
+		plan2 = document.getElementById("plan_2");
 	} catch(error) {
 		console.log(error);
 	}
+						
+	// plan display toggle
+	planToggle.onchange = function() {
+		try	{
+			if(this.checked) {
+				plan1.checked = true;
+				row.style.display = "";
+			}else {
+				row.style.display = "none";
+				plan1.checked, plan2.checked = false;			
+			} 
+		}catch(error) {
+			console.log(error);
+		}
+	};
+		
+	// plan1 radio switch
+	plan1.onchange = function() {
+		if(this.checked) plan2.checked = false;
+	};
+
+	// plan2 radio switch
+	plan2.onchange = function() {
+		if(this.checked) plan1.checked = false;
+	};
+	
 };
+
